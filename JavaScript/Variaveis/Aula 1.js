a7fdec8c55725a500bb43621ad3e48514c3ed290
Vinicius Sibin <vinisibim@gmail.com>
2026-01-17 12:16:59 -0300

    Iniciando o curso de Javascript, feito aula 1 sobre variáveis

---
+++ JavaScript/Variaveis/Aula 1.js
@@ -0,0 +1,5 @@
+const aluno = "João"; //Variável do tipo constante, não pode ser alterada durante o código.
+let professora = "Maria"; //Variável do tipo let, pode ser alterada durante o código.
+var exemplo; //Variável do tipo var, pode ser alterada durante o código. Porém foi descontinuada nas boas práticas. 
+
+console.log('Aluno:' + aluno + '\nProfessora:' + professora); //Exibe o valor das variáveis no console.
